78118f3b3cd0c31c67d6808d14cc3fe1938e857d
D11110 <danielaugustoma@hotmail.com>
2020-05-22 13:48:04 -0600

    Inicio de desarrollo para registro de usuario

--- src/lab4_joshuamartinez_danielmorales/Lab4_JoshuaMartinez_DanielMorales.java
+++ src/lab4_joshuamartinez_danielmorales/Lab4_JoshuaMartinez_DanielMorales.java
@@ -1,21 +1,93 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab4_joshuamartinez_danielmorales;
 
-/**
- *
- * @author danie
- */
-public class Lab4_JoshuaMartinez_DanielMorales {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Date;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Lab4_JoshuaMartinez_DanielMorales {
+    
+    static Scanner input =  new Scanner(System.in);
+    static ArrayList usuarios = new ArrayList();
+
+    public static void main(String[] args) throws ParseException {
+        char resp = 's';
+        while (resp=='s'||resp=='S') {
+            System.out.println("Bienvenido");
+            System.out.println("1. Iniciar sesion");
+            System.out.println("2. Registrarse");
+            System.out.println("3. Salir");
+            int op = input.nextInt();
+            switch (op) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    System.out.println("Bienvenido a la creacion de usuario");
+                    System.out.println("Ingrese su nombre:");
+                    String nombre = input.nextLine();
+                    input = new Scanner(System.in);
+                    System.out.println("Ingrese su grupo sanguineo: ");
+                    String grupoSang = input.nextLine();
+                    input = new Scanner(System.in);
+                    System.out.println("Ingrese el sexo: 1. Masculino 2. Femenino");
+                    String sexo = "";
+                    int opSexo = input.nextInt();
+                    switch (opSexo) {
+                        case 1:
+                            sexo = "Masculino";
+                            break;
+                        case 2:
+                            sexo = "Femenino";
+                            break;
+                        default:
+                            System.out.println("Ingrese una opcion valida");
+                    }
+                    System.out.println("Ingrese altura: ");
+                    double altura = input.nextDouble();
+                    System.out.println("Ingrese peso: ");
+                    int peso = input.nextInt();
+                    System.out.println("Ingrese su correo electronico: ");
+                    String correo = input.nextLine();
+                    input = new Scanner(System.in);
+                    System.out.println("Ingrese su usuario: ");
+                    String usuario = input.next();
+                    boolean userOrigCheck = false;
+                    if (usuarios.contains(usuario)) {
+                        System.out.println("Usuario ya existente");
+                        while (userOrigCheck == false) {
+                            System.out.println("Ingrese un usuario: ");
+                            usuario = input.next();
+                            if (usuarios.contains(usuario)) {
+                                System.out.println("Usuario ya existente");
+                            } else {
+                                userOrigCheck = true;
+                            }
+                        }
+                    }
+                    System.out.println("Cuantos idiomas habla? ");
+                    int cantIdiomas = input.nextInt();
+                    ArrayList idiomas = new ArrayList();
+                    for (int i = 0; i < cantIdiomas; i++) {
+                        System.out.println("Ingrese el " + i+1 + " idioma:" );
+                        String idioma = input.next();
+                        idiomas.add(idioma);
+                    }
+                    System.out.println("Ingrese su contraseña: ");
+                    String contraseña = input.next();
+                    System.out.println("Ingrese su fecha de nacimiento: (dd/MM/yyyy)");
+                    String fecha = input.nextLine();
+                    input = new Scanner(System.in);
+                    SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
+                    Date fecha_nacimiento = sd.parse(fecha);
+                    
+                    
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
     }
     
 }
